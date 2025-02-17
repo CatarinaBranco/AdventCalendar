d5726b9e6ac758824b8729e1a47ffde49477c72c
CatarinaBranco <ciabranco94@gmail.com>
2025-02-17 22:21:38 +0000

    Solve

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
 
 
 # content per day
